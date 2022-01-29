5ae6c93939c52474b784409401dff11af70b4d6e
Peter <peter.zmilczak@gmail.com>
2022-01-29 15:40:56 +0100

    fixed initProject IT

--- logchange-maven-plugin/src/test/java/dev/logchange/maven_plugin/mojo/InitProjectMojoIT.java
+++ logchange-maven-plugin/src/test/java/dev/logchange/maven_plugin/mojo/InitProjectMojoIT.java
@@ -4,6 +4,9 @@ import com.soebes.itf.jupiter.extension.MavenGoal;
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
 import com.soebes.itf.jupiter.extension.MavenTest;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.File;
 
 import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 
@@ -12,31 +15,29 @@ class InitProjectMojoIT {
 
     @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:init")
     @MavenTest
+    @DisplayName("Project with pom.xml not containing plugin, not init before, when init, all files should be created")
     void pomNotContainsPluginAndProjectNotInit(MavenExecutionResult result) {
         assertThat(result).isSuccessful()
                 .project()
                 .has("changelog")
                 .has("changelog\\unreleased");
 
-//        assertThat(result).isSuccessful()
-//                .project()
-//                .withFile("changelog\\unreleased\\.gitkeep")
-//                .exists();
-//
-//        assertThat(result)
-//                .project()
-//                .withFile("CHANGELOG.md")
-//                .exists();
-//
-//        assertThat(result)
-//                .out()
-//                .plain()
-//                .contains("[INFO] Initialize project for keep-changelog maven plugin\n" +
-//                        "[INFO] Created: CHANGELOG.md\n" +
-//                        "[INFO] Created: changelog\n" +
-//                        "[INFO] Created: unreleased\n" +
-//                        "[INFO] Created: .gitkeep\n" +
-//                        "[INFO] Initialize project successful");
+
+        File gitKeep = new File(result.getMavenProjectResult().getTargetProjectDirectory(), "changelog\\unreleased\\.gitkeep");
+        File changelog = new File(result.getMavenProjectResult().getTargetProjectDirectory(), "CHANGELOG.md");
+
+       assertThat(gitKeep).exists();
+       assertThat(changelog).exists();
+
+        assertThat(result)
+                .out()
+                .plain()
+                .contains("[INFO] Initialize project for logchange-maven-plugin",
+                        "[INFO] Created: CHANGELOG.md",
+                        "[INFO] Created: changelog",
+                        "[INFO] Created: unreleased",
+                        "[INFO] Created: .gitkeep",
+                        "[INFO] Initialize project successful");
     }
 
 }
