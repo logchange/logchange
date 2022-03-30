69f141103fe636415adfc9bc2a67f4216a8bbc10
Peter <peter.zmilczak@gmail.com>
2022-03-30 22:48:32 +0200

    AddChangelogEntryMojo implementing next inputs

--- logchange-maven-plugin/src/main/java/dev/logchange/maven_plugin/mojo/add/AddChangelogEntryMojo.java
+++ logchange-maven-plugin/src/main/java/dev/logchange/maven_plugin/mojo/add/AddChangelogEntryMojo.java
@@ -17,6 +17,7 @@ import org.codehaus.plexus.components.interactivity.PrompterException;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -98,10 +99,10 @@ public class AddChangelogEntryMojo extends AbstractMojo {
                 .type(getType())
                 .mergeRequests(getMergeRequests())
                 .issues(getIssues())
-                .links(null)
-                .authors(null)
-                .importantNotes(null)
-                .configurations(null)
+                .links(getLinks())
+                .authors(getAuthors())
+                .importantNotes(getNotes())
+                .configurations(getConfigurations())
                 .build();
     }
 
@@ -161,13 +162,13 @@ public class AddChangelogEntryMojo extends AbstractMojo {
 
     private List<ChangelogEntryLink> getLinks() throws PrompterException {
         String prompt = "Is there any links you want to include? [Y/y - YES] [N/n - NO] [press ENTER to skip] ";
+        List<ChangelogEntryLink> links = new ArrayList<>();
 
         while (true) {
             try {
                 String response = prompter.prompt(prompt);
                 if (response.trim().equalsIgnoreCase("Y")) {
-                    String name = prompter.prompt("Give a link caption or press ENTER to skip");
-                    //TODO
+                    return getLinksRecur(links);
                 } else {
                     return Collections.emptyList();
                 }
@@ -175,5 +176,86 @@ public class AddChangelogEntryMojo extends AbstractMojo {
                 prompter.showMessage(e.getMessage());
             }
         }
+    }
+
+    private List<ChangelogEntryLink> getLinksRecur(List<ChangelogEntryLink> links) throws PrompterException {
+        String name = prompter.prompt("Give a link caption or press ENTER to skip");
+        String link = prompter.prompt("Give a link");
+        links.add(ChangelogEntryLink.of(name, link));
+
+        if (prompter.prompt("Is there any other links you want to include? [Y/y - YES] [N/n - NO]").trim().equalsIgnoreCase("Y")) {
+            return getLinksRecur(links);
+        } else {
+            return links;
+        }
+    }
+
+    private List<ChangelogEntryAuthor> getAuthors() throws PrompterException {
+        String prompt = "Is there any authors of this change, that you want to include? [Y/y - YES] [N/n - NO] [press ENTER to skip] ";
+        List<ChangelogEntryAuthor> authors = new ArrayList<>();
+
+        while (true) {
+            try {
+                String response = prompter.prompt(prompt);
+                if (response.trim().equalsIgnoreCase("Y")) {
+                    return getAuthorsRecur(authors);
+                } else {
+                    return Collections.emptyList();
+                }
+            } catch (Exception e) {
+                prompter.showMessage(e.getMessage());
+            }
+        }
+    }
+
+    private List<ChangelogEntryAuthor> getAuthorsRecur(List<ChangelogEntryAuthor> authors) throws PrompterException {
+        try {
+            String name = prompter.prompt("Give a name of author or press ENTER to skip");
+            String nick = prompter.prompt("Give a nickname of author or press ENTER to skip");
+            String url = prompter.prompt("Give a url of author profile or press ENTER to skip");
+            authors.add(ChangelogEntryAuthor.of(name, nick, url));
+        } catch (IllegalArgumentException e) {
+            prompter.showMessage(e.getMessage());
+            return getAuthorsRecur(authors);
+        }
+
+        if (prompter.prompt("Is there any other links you want to include? [Y/y - YES] [N/n - NO]").trim().equalsIgnoreCase("Y")) {
+            return getAuthorsRecur(authors);
+        } else {
+            return authors;
+        }
+    }
+
+    private List<String> getNotes() throws PrompterException {
+        String prompt = "Is there any important information about this change (f.e. it affects other system) [Y/y - YES] [N/n - NO] [press ENTER to skip] ";
+        List<String> notes = new ArrayList<>();
+
+        while (true) {
+            try {
+                String response = prompter.prompt(prompt);
+                if (response.trim().length() > 0) {
+                    return getNotesRecur(notes);
+                } else {
+                    return Collections.emptyList();
+                }
+            } catch (Exception e) {
+                prompter.showMessage(e.getMessage());
+            }
+        }
+    }
+
+    private List<String> getNotesRecur(List<String> notes) throws PrompterException {
+        String note = prompter.prompt("Give a note");
+        notes.add(note.trim());
+
+        if (prompter.prompt("Is there any other note you want to include? [Y/y - YES] [N/n - NO]").trim().equalsIgnoreCase("Y")) {
+            return getNotesRecur(notes);
+        } else {
+            return notes;
+        }
+    }
+
+    private List<ChangelogEntryConfiguration> getConfigurations() {
+        return Collections.emptyList();
     }
 }
