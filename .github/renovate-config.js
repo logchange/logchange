7ef4b833fece9acba858e932d8c221935aed522f
Peter Zmilczak <peter.zmilczak@gmail.com>
2023-06-23 14:20:38 +0200

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -3,5 +3,11 @@ module.exports = {
   platform: 'github',
   repositories: [
     'logchange/logchange'
-  ]
+  ],
+  allowPostUpgradeCommandTemplating: true,
+  // postUpgradeTasks: {
+  //   commands: ['npx renovate-changelog-updater --depName {{{depName}}} --current-version {{{currentVersion}}} --new-version {{{newVersion}}}'],
+  //   fileFilters: ['**/*.yml', '**/*.yaml'],
+  //   executionMode: 'update',
+  // }
 };
