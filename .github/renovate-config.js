1ce6d10d790f827ba159b2a59f2e600d39e1d18e
Peter <peter.zmilczak@gmail.com>
2023-07-08 16:52:38 +0200

    add schedule and on trigger

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -7,7 +7,9 @@ module.exports = {
   allowPostUpgradeCommandTemplating: true,
   postUpgradeTasks: {
     //commands: ['curl -sSL https://raw.githubusercontent.com/logchange/logchange/main/add_entry.sh | bash -s -- -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
-    commands: ['install-tool maven && mvn dev.logchange:logchange-maven-plugin:add --non-recursive -DbatchMode -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depNameLinked}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
+    commands: [
+      'install-tool maven 3.9.2',
+      'mvn dev.logchange:logchange-maven-plugin:add --non-recursive -DbatchMode -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depNameLinked}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
     fileFilters: ['**/*.yml'],
     executionMode: 'update',
   }
