22f09742b9509efbe6dbf782351e7fc5ecdba9e7
Peter <peter.zmilczak@gmail.com>
2023-07-08 14:54:58 +0200

    test renovate postUpgrade tasts

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -6,7 +6,7 @@ module.exports = {
   ],
   allowPostUpgradeCommandTemplating: true,
   postUpgradeTasks: {
-    commands: ['curl -sSL https://raw.githubusercontent.com/logchange/logchange/main/add_entry.sh | bash -s -- -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depNameLinked}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
+    commands: ['curl -sSL https://raw.githubusercontent.com/logchange/logchange/main/add_entry.sh | bash -s -- -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
     fileFilters: ['**/*.yml'],
     executionMode: 'update',
   }
