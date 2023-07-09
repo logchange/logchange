3914d6688c0629d0e31e4100113ee468f974f5d0
Peter <peter.zmilczak@gmail.com>
2023-07-09 06:14:41 +0200

    add schedule and on trigger

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -8,7 +8,7 @@ module.exports = {
   postUpgradeTasks: {
     //commands: ['curl -sSL https://raw.githubusercontent.com/logchange/logchange/main/add_entry.sh | bash -s -- -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
     commands: [
-      'install-tool maven latest',
+      'install-tool maven 3.9.3',
       'mvn dev.logchange:logchange-maven-plugin:add --non-recursive -DbatchMode -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
     fileFilters: ['**/*.yml'],
     executionMode: 'update',
