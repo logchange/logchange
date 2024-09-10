83646f8bd1bdacf7e09047c00b961c3b261220f8
witx98 <witx90@gmail.com>
2024-09-10 19:37:02 +0200

    Added tests for lint command

--- logchange-maven-plugin/src/test/java/dev/logchange/maven_plugin/mojo/lint/LintChangelogMojoIT.java
+++ logchange-maven-plugin/src/test/java/dev/logchange/maven_plugin/mojo/lint/LintChangelogMojoIT.java
@@ -16,7 +16,79 @@ public class LintChangelogMojoIT {
 
     @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:lint")
     @MavenTest
-    @DisplayName("Project with pom.xml, task.yml with syntax error in changelog dir after release task.yml is still in unreleased dir")
+    @DisplayName("Project with pom.xml, task.yml and logchange-config.yml in changelog directory - lint succeeds and nothing is moved or generated")
+    void lintWithLogchangeConfig(MavenExecutionResult result) {
+        assertThat(result).isSuccessful()
+                .project()
+                .has("changelog")
+                .has("changelog/unreleased");
+
+
+        File gitKeep = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/.gitkeep");
+        File originalTask = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/task.yml");
+        File changelog = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "CHANGELOG.md");
+        File taskMovedToRelease = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/task.yml");
+        File releaseDateFile = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/release-date.txt");
+        File versionSummary = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/version-summary.md");
+
+        assertThat(gitKeep).exists();
+        assertThat(originalTask).exists();
+        assertThat(changelog).doesNotExist();
+        assertThat(taskMovedToRelease).doesNotExist();
+        assertThat(releaseDateFile).doesNotExist();
+        assertThat(versionSummary).doesNotExist();
+    }
+
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:lint")
+    @MavenTest
+    @DisplayName("Project with pom.xml and task.yml in changelog directory - lint succeeds and nothing is moved or generated")
+    void lintWithMissingLogchangeConfig(MavenExecutionResult result) {
+        assertThat(result).isSuccessful()
+                .project()
+                .has("changelog")
+                .has("changelog/unreleased");
+
+
+        File gitKeep = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/.gitkeep");
+        File originalTask = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/task.yml");
+        File changelog = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "CHANGELOG.md");
+        File taskMovedToRelease = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/task.yml");
+        File releaseDateFile = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/release-date.txt");
+        File versionSummary = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/version-summary.md");
+
+        assertThat(gitKeep).exists();
+        assertThat(originalTask).exists();
+        assertThat(changelog).doesNotExist();
+        assertThat(taskMovedToRelease).doesNotExist();
+        assertThat(releaseDateFile).doesNotExist();
+        assertThat(versionSummary).doesNotExist();
+    }
+
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:lint")
+    @MavenTest
+    @DisplayName("Project with pom.xml without changelog directory - lint fails and nothing is moved or generated")
+    void lintWithoutChangelogDirectory(MavenExecutionResult result) {
+        assertThat(result).isFailure();
+
+
+        File gitKeep = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/.gitkeep");
+        File originalTask = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/task.yml");
+        File changelog = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "CHANGELOG.md");
+        File taskMovedToRelease = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/task.yml");
+        File releaseDateFile = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/release-date.txt");
+        File versionSummary = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/version-summary.md");
+
+        Assertions.assertThat(gitKeep).doesNotExist();
+        Assertions.assertThat(originalTask).doesNotExist();
+        Assertions.assertThat(changelog).doesNotExist();
+        Assertions.assertThat(taskMovedToRelease).doesNotExist();
+        Assertions.assertThat(releaseDateFile).doesNotExist();
+        Assertions.assertThat(versionSummary).doesNotExist();
+    }
+
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:lint")
+    @MavenTest
+    @DisplayName("Project with pom.xml, task.yml with syntax error in changelog directory - lint fails and nothing is moved or generated")
     void lintWithYMLSyntaxError(MavenExecutionResult result) {
         assertThat(result).isFailure()
                 .project()
@@ -25,8 +97,8 @@ public class LintChangelogMojoIT {
 
 
         File gitKeep = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/.gitkeep");
-        File changelog = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "CHANGELOG.md");
         File originalTask = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/unreleased/task.yml");
+        File changelog = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "CHANGELOG.md");
         File taskMovedToRelease = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/task.yml");
         File releaseDateFile = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/release-date.txt");
         File versionSummary = new File(result.getMavenProjectResult().getTargetProjectDirectory().toString(), "changelog/v1.0.0/version-summary.md");
