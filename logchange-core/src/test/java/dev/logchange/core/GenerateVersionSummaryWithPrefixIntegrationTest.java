ae1161844a179affaf90f9e07f1c6394e935812c
witx98 <witx90@gmail.com>
2024-10-30 21:50:47 +0100

    Added GenerateVersionSummaryWithPrefix Integration Test

---
+++ logchange-core/src/test/java/dev/logchange/core/GenerateVersionSummaryWithPrefixIntegrationTest.java
@@ -0,0 +1,74 @@
+package dev.logchange.core;
+
+import dev.logchange.core.application.changelog.repository.AggregatedVersionQuery;
+import dev.logchange.core.application.changelog.repository.VersionSummaryRepository;
+import dev.logchange.core.application.changelog.service.aggregate.AggregateProjectsVersionService;
+import dev.logchange.core.application.file.query.TarGzQuery;
+import dev.logchange.core.domain.changelog.command.AggregateProjectsVersionUseCase;
+import dev.logchange.core.domain.changelog.command.AggregateProjectsVersionUseCase.AggregateChangelogsVersionsCommand;
+import dev.logchange.core.domain.config.model.Config;
+import dev.logchange.core.domain.config.model.aggregate.AggregatedProject;
+import dev.logchange.core.domain.config.model.aggregate.AggregatedProjectType;
+import dev.logchange.core.domain.config.model.aggregate.Aggregates;
+import dev.logchange.core.infrastructure.persistance.changelog.FileVersionSummaryRepository;
+import dev.logchange.core.infrastructure.query.changelog.FileAggregatedVersionFinder;
+import dev.logchange.core.infrastructure.query.file.FileReader;
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GenerateVersionSummaryWithPrefixIntegrationTest {
+
+    private static final String PATH = "src/test/resources/GenerateVersionSummaryWithPrefixIntegrationTest/";
+
+
+    @Test
+    void shouldMatchExpectedChangelog() throws IOException {
+        //given:
+        File changelogInputDir = new File(PATH + "changelog");
+        String VERSION_DIR = PATH + "changelog/v1.0.0/";
+        File expectedChangelogOutputFile = new File(VERSION_DIR + "expected-version-summary.md");
+
+        AggregateChangelogsVersionsCommand command = AggregateChangelogsVersionsCommand.of(prepareAggregates(), "1.0.0");
+        VersionSummaryRepository vsr = new FileVersionSummaryRepository(changelogInputDir, Config.EMPTY);
+        AggregatedVersionQuery avq = new FileAggregatedVersionFinder(command.getVersion(), new FileReader());
+        TarGzQuery tarGzQuery = new FakeTarGzExtractor();
+        AggregateProjectsVersionUseCase apv = new AggregateProjectsVersionService(avq, vsr, tarGzQuery);
+
+        //when:
+        apv.handle(command);
+        File versionSummaryFile = new File(VERSION_DIR + "version-summary.md");
+
+        //then:
+
+
+        String expectedContent = FileUtils.fileRead(expectedChangelogOutputFile);
+        String actualContent = FileUtils.fileRead(versionSummaryFile);
+        assertThat(actualContent).isEqualToIgnoringNewLines(expectedContent);
+    }
+
+    Aggregates prepareAggregates() {
+        List<AggregatedProject> aggregatedProject = new ArrayList<>();
+        aggregatedProject.add(new AggregatedProject("TEST_PROJECT", "URL", AggregatedProjectType.TAR_GZ, "changelog"));
+        return Aggregates.builder()
+                .projects(aggregatedProject)
+                .build();
+    }
+
+
+    static class FakeTarGzExtractor implements TarGzQuery {
+
+        @Override
+        public Path get(String projectUrl, String projectInputDir) throws IOException {
+            return Paths.get("../../../../resources/GenerateVersionSummaryWithPrefixIntegrationTest/extracted/changelog");
+        }
+    }
+}
---
+++ logchange-core/src/test/resources/GenerateVersionSummaryIntegrationTest/extracted/changelog/v1.0.1/version-summary.md
@@ -0,0 +1,51 @@
+<!-- @formatter:off -->
+<!-- noinspection -->
+<!-- Prevents auto format, for JetBrains IDE File > Settings > Editor > Code Style (Formatter Tab) > Turn formatter on/off with markers in code comments  -->
+
+<!-- This file is automatically generate by logchange tool 🌳 🪓 => 🪵 -->
+<!-- Visit https://github.com/logchange/logchange and leave a star 🌟 -->
+<!-- !!! ⚠️ DO NOT MODIFY THIS FILE, YOUR CHANGES WILL BE LOST ⚠️ !!! -->
+
+
+[1.0.1] - 2021-01-03
+--------------------
+
+### Important notes
+
+- Update style.css on server during instalation 1
+- configuration 2
+- configuration 3
+- Update style.css on server during instalation
+- Update style.css on server during instalation
+- some realy important recomendation
+
+### Added (1 change)
+
+- Test title !1 #1 [TASK_NUMBER](https://www.google.pl) ([Piotr](https://github.com/marwin1991) @marwin1991)
+
+### Removed (2 changes)
+
+- Test title !1 #1 [TASK_NUMBER](https://www.google.pl) (Piotr)
+- Test title !1 #1 #2 [TASK_NUMBER](https://www.google.pl) [other link](https://www.google.pl) (@marwin1991) ([Piotr](https://github.com/marwin1991) @marwin1991)
+
+### Fixed (1 change)
+
+- Test title !1 #1 [TASK_NUMBER](https://www.google.pl) (@marwin1991 [LINK](https://github.com/marwin1991))
+
+### Configuration changes
+
+| Type: database parameter                                                                                                          |
+| --------------------------------------------------------------------------------------------------------------------------------- |
+| <ul><li>Added `aaa.some.value` with default value: `true`</li><li>Description: Enables displying feature name</li></ul>           |
+| <ul><li>Added `simpleparam.name.enabled` with default value: `true`</li><li>Description: Enables displying feature name</li></ul> |
+| <ul><li>Added `simpleparam.name.enabled` with default value: `true`</li><li>Description: Enables displying feature name</li></ul> |
+| <ul><li>Added `simpleparam.name.enabled` with default value: `true`</li><li>Description: Enables displying feature name</li></ul> |
+| <ul><li>Added `simpleparam.name.enabled` with default value: `true`</li><li>Description: Enables displying feature name</li></ul> |
+| <ul><li>Updated `aaa.some.value` with default value: `true`</li><li>Description: Enables displying feature name</li></ul>         |
+| <ul><li>Deleted `aaa.some.value` with default value: `true`</li><li>Description: Enables displying feature name</li></ul>         |
+
+| Type: a docker-compose                                                                                                  |
+| ----------------------------------------------------------------------------------------------------------------------- |
+| <ul><li>Added `aaa.some.value` with default value: `true`</li><li>Description: Enables displying feature name</li></ul> |
+
+
