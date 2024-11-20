3d667b307295fdb902c82eed6f0503aa692a1c97
witx98 <witx90@gmail.com>
2024-11-20 19:44:02 +0100

    Serialization fix

--- logchange-core/src/main/java/dev/logchange/core/format/yml/config/templates/YMLTemplates.java
+++ logchange-core/src/main/java/dev/logchange/core/format/yml/config/templates/YMLTemplates.java
@@ -3,11 +3,15 @@ package dev.logchange.core.format.yml.config.templates;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.logchange.core.domain.config.model.templates.Templates;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 
 @Log
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class YMLTemplates {
 
     @JsonProperty(index = 0)
