ec2737eae490a291e39894af0b1414a083726dc6
marwin1991 <peter.zmilczak@gmail.com>
2025-01-22 23:05:34 +0100

    added test to fix https://github.com/logchange/logchange/commit/d030288f19bfd41cb376bcf06c17eea04cf70c63

--- logchange-commands/src/test/java/dev/logchange/commands/generate/GenerateProjectCommandTest.java
+++ logchange-commands/src/test/java/dev/logchange/commands/generate/GenerateProjectCommandTest.java
@@ -1,7 +1,6 @@
 package dev.logchange.commands.generate;
 
 import org.codehaus.plexus.util.FileUtils;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -20,29 +19,52 @@ class GenerateProjectCommandTest {
     private static final String TEST_FILE = "test-entry.yml";
     private static final String OUTPUT_FILE = "CHANGELOG.md";
 
-    @AfterEach
-    void cleanup() {
-        new File(PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + "version-summary.md").delete();
-        new File(PATH + "/" + OUTPUT_FILE).delete();
-    }
-
     @Test
-    void shouldGenerateProject() throws IOException {
+    void onlyUnreleasedDir() throws IOException {
         // given:
-        File entry = new File(PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + TEST_FILE);
-        File outputFile = new File(PATH + "/" + OUTPUT_FILE);
-        File expectedChangelog = new File(PATH + "/" + "EXPECTED_CHANGELOG.md");
+        String TEST_PATH = PATH + "/onlyUnreleasedDir";
+        File entry = new File(TEST_PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + TEST_FILE);
+        File outputFile = new File(TEST_PATH + "/" + OUTPUT_FILE);
+        File expectedChangelog = new File(TEST_PATH + "/" + "EXPECTED_CHANGELOG.md");
         assertTrue(entry.exists());
         assertFalse(outputFile.exists());
         assertTrue(expectedChangelog.exists());
 
         // when:
-        GenerateProjectCommand.of(PATH, INPUT_DIR, PATH + "/" + OUTPUT_FILE, CONFIG_FILE).execute(false);
+        GenerateProjectCommand.of(TEST_PATH, INPUT_DIR, TEST_PATH + "/" + OUTPUT_FILE, CONFIG_FILE).execute(false);
 
         // then:
         assertTrue(outputFile.exists());
         String expectedContent = FileUtils.fileRead(expectedChangelog, "UTF-8");
         String actualContent = FileUtils.fileRead(outputFile, "UTF-8");
         assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
+
+        // cleanup:
+        new File(TEST_PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + "version-summary.md").delete();
+        new File(TEST_PATH + "/" + OUTPUT_FILE).delete();
+    }
+
+    @Test
+    void missingReleaseDateTxt() throws IOException {
+        // given:
+        String TEST_PATH = PATH + "/missingReleaseDateTxt";
+        File entry = new File(TEST_PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + TEST_FILE);
+        File outputFile = new File(TEST_PATH + "/" + OUTPUT_FILE);
+        File expectedChangelog = new File(TEST_PATH + "/" + "EXPECTED_CHANGELOG.md");
+        assertTrue(entry.exists());
+        assertFalse(outputFile.exists());
+        assertTrue(expectedChangelog.exists());
+
+        // when:
+        GenerateProjectCommand.of(TEST_PATH, INPUT_DIR, TEST_PATH + "/" + OUTPUT_FILE, CONFIG_FILE).execute(false);
+
+        // then:
+        assertTrue(outputFile.exists());
+        String expectedContent = FileUtils.fileRead(expectedChangelog, "UTF-8");
+        String actualContent = FileUtils.fileRead(outputFile, "UTF-8");
+        assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
+
+        // cleanup:
+        new File(TEST_PATH + "/" + OUTPUT_FILE).delete();
     }
 }
---
+++ logchange-commands/src/test/resources/GenerateProjectCommandTest/missingReleaseDateTxt/EXPECTED_CHANGELOG.md
@@ -0,0 +1,30 @@
+<!-- @formatter:off -->
+<!-- noinspection -->
+<!-- Prevents auto format, for JetBrains IDE File > Settings > Editor > Code Style (Formatter Tab) > Turn formatter on/off with markers in code comments  -->
+
+<!-- This file is automatically generate by logchange tool 🌳 🪓 => 🪵 -->
+<!-- Visit https://github.com/logchange/logchange and leave a star 🌟 -->
+<!-- !!! ⚠️ DO NOT MODIFY THIS FILE, YOUR CHANGES WILL BE LOST ⚠️ !!! -->
+
+
+[unreleased]
+------------
+
+### Added (1 change)
+
+- title !1000 #100 [name](url) ([NAME](URL) @NICK)
+
+
+[1.0.0]
+-------
+
+### Added (1 change)
+
+- Test title !1 #1 [TASK_NUMBER](https://www.google.pl) (Piotr)
+
+### Fixed (1 change)
+
+- Test title !1 #1 (@marwin1991)
+
+
+
