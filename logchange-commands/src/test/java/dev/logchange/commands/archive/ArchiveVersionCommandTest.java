e14cdd454f45d7e2fbd538b8986435177f07d62c
witx98 <witx90@gmail.com>
2025-03-17 21:19:07 +0100

    Added Archive version command test

--- logchange-commands/src/test/java/dev/logchange/commands/archive/ArchiveVersionCommandTest.java
+++ logchange-commands/src/test/java/dev/logchange/commands/archive/ArchiveVersionCommandTest.java
@@ -12,24 +12,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ArchiveVersionCommandTest {
 
+    private static final String EXPECTED_ARCHIVE_MD = "EXPECTED_ARCHIVE.md";
     private static final String PATH = "src/test/resources/ArchiveVersionCommandTest";
     private static final String INPUT_DIR = "changelog";
     private static final String CONFIG_FILE = "logchange-config.yml";
     private static final String ARCHIVE_FILE = "archive.md";
 
     @Test
-    void onlyUnreleasedDir() throws IOException {
+    void whenArchiveNotExists() throws IOException {
         // given:
-        String TEST_PATH = PATH + "/whenArchiveDoseNotExists";
+        String TEST_PATH = PATH + "/whenArchiveNotExists";
+        File unreleasedDir = new File(TEST_PATH + "/" + INPUT_DIR + "/unreleased");
         File versionDir = new File(TEST_PATH + "/" + INPUT_DIR + "/v1.0.1");
         File secondVersionDir = new File(TEST_PATH + "/" + INPUT_DIR + "/v1.0.2");
-        File archiveVersionFile = new File(TEST_PATH + "/" + INPUT_DIR + "/archive-1.0.0.md");
+        File thirdVersionDir = new File(TEST_PATH + "/" + INPUT_DIR + "/v1.0.3");
+        File archivedVersionFile = new File(TEST_PATH + "/" + INPUT_DIR + "/archive-1.0.0.md");
         File archiveFile = new File(TEST_PATH + "/" + INPUT_DIR + "/" + ARCHIVE_FILE);
-        File expectedArchive = new File(TEST_PATH + "/" + "EXPECTED_ARCHIVE.md");
+        File expectedArchive = new File(TEST_PATH + "/" + EXPECTED_ARCHIVE_MD);
 
-        assertTrue(archiveVersionFile.exists());
+        assertTrue(unreleasedDir.exists());
+        assertTrue(archivedVersionFile.exists());
         assertTrue(versionDir.exists());
         assertTrue(secondVersionDir.exists());
+        assertTrue(thirdVersionDir.exists());
         assertFalse(archiveFile.exists());
         assertTrue(expectedArchive.exists());
 
@@ -37,6 +42,11 @@ class ArchiveVersionCommandTest {
         ArchiveVersionCommand.of(TEST_PATH, INPUT_DIR, "1.0.2", CONFIG_FILE).execute(false);
 
         // then:
+        assertTrue(unreleasedDir.exists());
+        assertFalse(archivedVersionFile.exists());
+        assertFalse(versionDir.exists());
+        assertFalse(secondVersionDir.exists());
+        assertTrue(thirdVersionDir.exists());
         assertTrue(archiveFile.exists());
         String expectedContent = FileUtils.fileRead(expectedArchive, "UTF-8");
         String actualContent = FileUtils.fileRead(archiveFile, "UTF-8");
@@ -44,5 +54,43 @@ class ArchiveVersionCommandTest {
 
         // cleanup:
         new File(TEST_PATH + "/" + INPUT_DIR + "/" + ARCHIVE_FILE).delete();
+    }
+
+    @Test
+    void whenArchiveExists() throws IOException {
+        // given:
+        String TEST_PATH = PATH + "/whenArchiveExists";
+        File unreleasedDir = new File(TEST_PATH + "/" + INPUT_DIR + "/unreleased");
+        File versionDir = new File(TEST_PATH + "/" + INPUT_DIR + "/v1.0.1");
+        File secondVersionDir = new File(TEST_PATH + "/" + INPUT_DIR + "/v1.0.2");
+        File thirdVersionDir = new File(TEST_PATH + "/" + INPUT_DIR + "/v1.0.3");
+        File archivedVersionFile = new File(TEST_PATH + "/" + INPUT_DIR + "/archive-1.0.0.md");
+        File archiveFile = new File(TEST_PATH + "/" + INPUT_DIR + "/" + ARCHIVE_FILE);
+        File expectedArchive = new File(TEST_PATH + "/" + EXPECTED_ARCHIVE_MD);
+
+        assertTrue(unreleasedDir.exists());
+        assertTrue(archivedVersionFile.exists());
+        assertTrue(versionDir.exists());
+        assertTrue(secondVersionDir.exists());
+        assertTrue(thirdVersionDir.exists());
+        assertTrue(archiveFile.exists());
+        assertTrue(expectedArchive.exists());
+
+        // when:
+        ArchiveVersionCommand.of(TEST_PATH, INPUT_DIR, "1.0.2", CONFIG_FILE).execute(false);
+
+        // then:
+        assertTrue(unreleasedDir.exists());
+        assertFalse(archivedVersionFile.exists());
+        assertFalse(versionDir.exists());
+        assertFalse(secondVersionDir.exists());
+        assertTrue(thirdVersionDir.exists());
+        assertTrue(archiveFile.exists());
+        String expectedContent = FileUtils.fileRead(expectedArchive, "UTF-8");
+        String actualContent = FileUtils.fileRead(archiveFile, "UTF-8");
+        assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
+
+        // cleanup:
+//        new File(TEST_PATH + "/" + INPUT_DIR + "/" + ARCHIVE_FILE).delete();
     }
 }
