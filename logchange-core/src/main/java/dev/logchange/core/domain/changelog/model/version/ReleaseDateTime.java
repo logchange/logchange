d0180b061dd3ea31b3ee44f10b74b0f83fef7f10
marwin1991 <peter.zmilczak@gmail.com>
2025-03-18 22:22:39 +0100

    null safe

--- logchange-core/src/main/java/dev/logchange/core/domain/changelog/model/version/ReleaseDateTime.java
+++ logchange-core/src/main/java/dev/logchange/core/domain/changelog/model/version/ReleaseDateTime.java
@@ -12,6 +12,9 @@ public class ReleaseDateTime implements Comparable<ReleaseDateTime> {
     private final OffsetDateTime value;
 
     public static ReleaseDateTime of(OffsetDateTime value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Release date value cannot be null!");
+        }
         return new ReleaseDateTime(value);
     }
 
@@ -22,6 +25,9 @@ public class ReleaseDateTime implements Comparable<ReleaseDateTime> {
 
     @Override
     public int compareTo(ReleaseDateTime o) {
+        if (o == null) {
+            return 1;
+        }
         return value.compareTo(o.value);
     }
 }
