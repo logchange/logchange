ac8abf4810f99656678d474cb7e8041d5facf581
witx98 <witx90@gmail.com>
2025-04-02 00:56:42 +0200

    Add validation for missing title and type in YAML changelog entries

--- logchange-core/src/main/java/dev/logchange/core/format/yml/changelog/entry/YMLChangelogInvalidConfigValuesException.java
+++ logchange-core/src/main/java/dev/logchange/core/format/yml/changelog/entry/YMLChangelogInvalidConfigValuesException.java
@@ -3,25 +3,24 @@ package dev.logchange.core.format.yml.changelog.entry;
 import java.util.Collection;
 
 public class YMLChangelogInvalidConfigValuesException extends RuntimeException {
-  public String path;
-  public Collection<String> invalidProperties;
+    public String path;
+    public Collection<String> invalidProperties;
 
-  public static String toString(String path, Collection<String> invalidProperties) {
-    StringBuilder sb = new StringBuilder();
-    sb.append("Errors in ").append(path).append(":\n");
+    public YMLChangelogInvalidConfigValuesException(String path, Collection<String> errors) {
+        super(toString(path, errors));
 
-    for (String error : invalidProperties) {
-        sb.append(error)
-          .append("\n");
+        this.path = path;
+        this.invalidProperties = errors;
     }
 
-    return sb.toString();
-  }
+    public static String toString(String path, Collection<String> invalidProperties) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Errors in ").append(path).append(":\n");
 
-  public YMLChangelogInvalidConfigValuesException(String path, Collection<String> errors) {
-    super(toString(path, errors));
+        for (String error : invalidProperties) {
+            sb.append("\t").append(error).append("\n");
+        }
 
-    this.path = path;
-    this.invalidProperties = errors;
-  }
+        return sb.toString();
+    }
 }
