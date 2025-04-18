8efe49d21ee302bb1ac63c3f7a5c9ca8aefc0c92
witx98 <witx90@gmail.com>
2025-04-18 17:50:40 +0200

    Added archivization tests

--- logchange-commands/src/test/java/dev/logchange/commands/init/InitProjectCommandTest.java
+++ logchange-commands/src/test/java/dev/logchange/commands/init/InitProjectCommandTest.java
@@ -15,33 +15,28 @@ class InitProjectCommandTest {
     private static final String CONFIG_FILE = "logchange-config.yml";
     private static final String GIT_KEEP_FILE = ".gitkeep";
     private static final String OUTPUT_FILE = "CHANGELOG.md";
-
-    @AfterEach
-    void cleanup() {
-        new File(PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + GIT_KEEP_FILE).delete();
-        new File(PATH + "/" + INPUT_DIR + "/" + UNRELEASED).delete();
-        new File(PATH + "/" + INPUT_DIR + "/" + CONFIG_FILE).delete();
-        new File(PATH + "/" + INPUT_DIR).delete();
-        new File(PATH + "/" + OUTPUT_FILE).delete();
-    }
+    private static final String ARCHIVE_FILE = "archive.md";
 
     @Test
     void shouldInitializeProject() {
         // given:
-        File gitKeepFile = new File(PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + GIT_KEEP_FILE);
-        File unreleasedDir = new File(PATH + "/" + INPUT_DIR + "/" + UNRELEASED);
-        File configFile = new File(PATH + "/" + INPUT_DIR + "/" + CONFIG_FILE);
-        File changelogDir = new File(PATH + "/" + INPUT_DIR);
-        File changelogFile = new File(PATH + "/" + OUTPUT_FILE);
+        String rootPath = PATH + "initializeProject/";
+        File gitKeepFile = new File(rootPath + INPUT_DIR + "/" + UNRELEASED + "/" + GIT_KEEP_FILE);
+        File unreleasedDir = new File(rootPath + INPUT_DIR + "/" + UNRELEASED);
+        File configFile = new File(rootPath + INPUT_DIR + "/" + CONFIG_FILE);
+        File changelogDir = new File(rootPath + INPUT_DIR);
+        File changelogFile = new File(rootPath + OUTPUT_FILE);
+        File archiveFile = new File(rootPath + INPUT_DIR + "/" + ARCHIVE_FILE);
 
         assertFalse(changelogDir.exists());
         assertFalse(changelogFile.exists());
         assertFalse(unreleasedDir.exists());
         assertFalse(configFile.exists());
         assertFalse(gitKeepFile.exists());
+        assertFalse(archiveFile.exists());
 
         // when:
-        InitProjectCommand.of(PATH, INPUT_DIR, UNRELEASED, OUTPUT_FILE).execute();
+        InitProjectCommand.of(rootPath, INPUT_DIR, UNRELEASED, OUTPUT_FILE).execute();
 
         // then:
         assertTrue(changelogDir.exists());
@@ -58,5 +53,62 @@ class InitProjectCommandTest {
 
         assertTrue(changelogFile.exists());
         assertTrue(changelogFile.isFile());
+
+        assertFalse(archiveFile.exists());
+
+        // cleanup:
+        gitKeepFile.delete();
+        unreleasedDir.delete();
+        configFile.delete();
+        changelogDir.delete();
+        changelogFile.delete();
+    }
+
+    @Test
+    void shouldInitializeProjectWithArchive() {
+        // given:
+        String rootPath = PATH + "initializeProjectWithArchive/";
+        File gitKeepFile = new File(rootPath + INPUT_DIR + "/" + UNRELEASED + "/" + GIT_KEEP_FILE);
+        File unreleasedDir = new File(rootPath + INPUT_DIR + "/" + UNRELEASED);
+        File configFile = new File(rootPath + INPUT_DIR + "/" + CONFIG_FILE);
+        File changelogDir = new File(rootPath + INPUT_DIR);
+        File changelogFile = new File(rootPath + OUTPUT_FILE);
+        File archiveFile = new File(rootPath + INPUT_DIR + "/" + ARCHIVE_FILE);
+
+        assertFalse(changelogDir.exists());
+        assertTrue(changelogFile.exists());
+        assertFalse(unreleasedDir.exists());
+        assertFalse(configFile.exists());
+        assertFalse(gitKeepFile.exists());
+        assertFalse(archiveFile.exists());
+
+        // when:
+        InitProjectCommand.of(rootPath, INPUT_DIR, UNRELEASED, OUTPUT_FILE).execute();
+
+        // then:
+        assertTrue(changelogDir.exists());
+        assertTrue(changelogDir.isDirectory());
+
+        assertTrue(unreleasedDir.exists());
+        assertTrue(unreleasedDir.isDirectory());
+
+        assertTrue(configFile.exists());
+        assertTrue(configFile.isFile());
+
+        assertTrue(gitKeepFile.exists());
+        assertTrue(gitKeepFile.isFile());
+
+        assertTrue(changelogFile.exists());
+        assertTrue(changelogFile.isFile());
+
+        assertTrue(archiveFile.exists());
+        assertTrue(archiveFile.isFile());
+
+        // cleanup:
+        gitKeepFile.delete();
+        unreleasedDir.delete();
+        configFile.delete();
+        archiveFile.delete();
+        changelogDir.delete();
     }
 }
