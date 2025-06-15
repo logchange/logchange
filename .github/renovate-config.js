c4a8091f384d15116b568d5f919654ce21bb801e
Peter Zmilczak <peter.zmilczak@gmail.com>
2025-06-15 21:37:47 +0200

    fix publishing to maven central and adjust gh action files names (#524)

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -9,7 +9,7 @@ module.exports = {
     //commands: ['curl -sSL https://raw.githubusercontent.com/logchange/logchange/main/add_entry.sh | bash -s -- -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}}'],
     commands: [
       'install-tool maven 3.9.3',
-      'mvn dev.logchange:logchange-maven-plugin:add --non-recursive -DbatchMode -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor=marwin1991 -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}} || true'],
+      'mvn dev.logchange:logchange-maven-plugin:add --non-recursive -DbatchMode -DfileName=renovate-{{{depNameSanitized}}}-{{{newVersion}}}.yml -Dtitle="Upgraded {{{depName}}} from {{{currentVersion}}} to {{{newVersion}}}" -Dauthor.name=logchange-bot;logchange-bot;team@logchange.dev -Dtype=dependency_update -Dlink.name=notes -Dlink.url={{{url}}} || true'],
     fileFilters: ['**/*.yml'],
     executionMode: 'update',
   }
