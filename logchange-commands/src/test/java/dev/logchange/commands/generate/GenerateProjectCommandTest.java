8c8df25cec44450265604112173abfbbb3d99287
marwin1991 <peter.zmilczak@gmail.com>
2025-11-02 21:12:54 +0100

    Refactor `GenerateProjectCommandTest` to use `@TempDir` and improve test resource handling with `Path#resolve` methods.

--- logchange-commands/src/test/java/dev/logchange/commands/generate/GenerateProjectCommandTest.java
+++ logchange-commands/src/test/java/dev/logchange/commands/generate/GenerateProjectCommandTest.java
@@ -1,115 +1,99 @@
 package dev.logchange.commands.generate;
 
-import org.codehaus.plexus.util.FileUtils;
+import dev.logchange.utils.TestResourcePath;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
+import static dev.logchange.utils.TestResourcePath.copyToTempDir;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GenerateProjectCommandTest {
 
-    private static final String PATH = "src/test/resources/GenerateProjectCommandTest";
+    private static final Path PATH = TestResourcePath.getPath(GenerateProjectCommandTest.class);
     private static final String INPUT_DIR = "changelog";
-    private static final String UNRELEASED = "unreleased/";
-    private static final String UNRELEASED_VERSIONED = "unreleased-1.2.3/";
+    private static final String UNRELEASED = "unreleased";
+    private static final String UNRELEASED_VERSIONED = "unreleased-1.2.3";
     private static final String CONFIG_FILE = "logchange-config.yml";
     private static final String TEST_FILE = "test-entry.yml";
     private static final String OUTPUT_FILE = "CHANGELOG.md";
 
     @Test
-    void onlyUnreleasedDir() throws IOException {
+    void onlyUnreleasedDir(@TempDir Path tempDir) throws IOException {
         // given:
-        String TEST_PATH = PATH + "/onlyUnreleasedDir";
-        try {
-            // given:
-            File entry = new File(TEST_PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + TEST_FILE);
-            File outputFile = new File(TEST_PATH + "/" + OUTPUT_FILE);
-            File expectedChangelog = new File(TEST_PATH + "/" + "EXPECTED_CHANGELOG.md");
-            assertTrue(entry.exists());
-            assertFalse(outputFile.exists());
-            assertTrue(expectedChangelog.exists());
+        Path VALID_PATH = copyToTempDir(PATH.resolve("onlyUnreleasedDir"), tempDir);
+        Path VALID_INPUT_DIR = VALID_PATH.resolve(INPUT_DIR);
 
-            // when:
-            GenerateProjectCommand.of(TEST_PATH, INPUT_DIR, TEST_PATH + "/" + OUTPUT_FILE, CONFIG_FILE).execute(false);
+        File entry = VALID_INPUT_DIR.resolve(UNRELEASED).resolve(TEST_FILE).toFile();
+        File outputFile = VALID_PATH.resolve(OUTPUT_FILE).toFile();
+        File expectedChangelog = VALID_PATH.resolve("EXPECTED_CHANGELOG.md").toFile();
 
-            // then:
-            assertTrue(outputFile.exists());
-            String expectedContent = FileUtils.fileRead(expectedChangelog, "UTF-8");
-            String actualContent = FileUtils.fileRead(outputFile, "UTF-8");
-            assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
-        } finally {
-            cleanupMarkdownFiles(Paths.get(TEST_PATH));
-        }
+        assertTrue(entry.exists());
+        assertFalse(outputFile.exists());
+        assertTrue(expectedChangelog.exists());
+
+        // when:
+        GenerateProjectCommand.of(VALID_PATH.toString(), INPUT_DIR, VALID_PATH.resolve(OUTPUT_FILE).toString(), CONFIG_FILE).execute(false);
+
+        // then:
+        assertTrue(outputFile.exists());
+        String expectedContent = new String(Files.readAllBytes(expectedChangelog.toPath()), StandardCharsets.UTF_8);
+        String actualContent = new String(Files.readAllBytes(outputFile.toPath()), StandardCharsets.UTF_8);
+        assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
     }
 
     @Test
-    void missingReleaseDateTxt() throws IOException {
+    void missingReleaseDateTxt(@TempDir Path tempDir) throws IOException {
         // given:
-        String TEST_PATH = PATH + "/missingReleaseDateTxt";
-        try {
-            // given:
-            File entry = new File(TEST_PATH + "/" + INPUT_DIR + "/" + UNRELEASED + "/" + TEST_FILE);
-            File outputFile = new File(TEST_PATH + "/" + OUTPUT_FILE);
-            File expectedChangelog = new File(TEST_PATH + "/" + "EXPECTED_CHANGELOG.md");
-            assertTrue(entry.exists());
-            assertFalse(outputFile.exists());
-            assertTrue(expectedChangelog.exists());
+        Path VALID_PATH = copyToTempDir(PATH.resolve("missingReleaseDateTxt"), tempDir);
+        Path VALID_INPUT_DIR = VALID_PATH.resolve(INPUT_DIR);
 
-            // when:
-            GenerateProjectCommand.of(TEST_PATH, INPUT_DIR, TEST_PATH + "/" + OUTPUT_FILE, CONFIG_FILE).execute(false);
+        File entry = VALID_INPUT_DIR.resolve(UNRELEASED).resolve(TEST_FILE).toFile();
+        File outputFile = VALID_PATH.resolve(OUTPUT_FILE).toFile();
+        File expectedChangelog = VALID_PATH.resolve("EXPECTED_CHANGELOG.md").toFile();
 
-            // then:
-            assertTrue(outputFile.exists());
-            String expectedContent = FileUtils.fileRead(expectedChangelog, "UTF-8");
-            String actualContent = FileUtils.fileRead(outputFile, "UTF-8");
-            assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
-        } finally {
-            cleanupMarkdownFiles(Paths.get(TEST_PATH));
-        }
+        assertTrue(entry.exists());
+        assertFalse(outputFile.exists());
+        assertTrue(expectedChangelog.exists());
+
+        // when:
+        GenerateProjectCommand.of(VALID_PATH.toString(), INPUT_DIR, VALID_PATH.resolve(OUTPUT_FILE).toString(), CONFIG_FILE).execute(false);
+
+        // then:
+        assertTrue(outputFile.exists());
+        String expectedContent = new String(Files.readAllBytes(expectedChangelog.toPath()), StandardCharsets.UTF_8);
+        String actualContent = new String(Files.readAllBytes(outputFile.toPath()), StandardCharsets.UTF_8);
+        assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
     }
 
     @Test
-    void versionedUnreleasedDir() throws IOException {
-        String TEST_PATH = PATH + "/versionedUnreleasedDir";
-        try {
-            // given:
-            File entry = new File(TEST_PATH + "/" + INPUT_DIR + "/" + UNRELEASED_VERSIONED + "/" + TEST_FILE);
-            File outputFile = new File(TEST_PATH + "/" + OUTPUT_FILE);
-            File expectedChangelog = new File(TEST_PATH + "/" + "EXPECTED_CHANGELOG.md");
-            assertTrue(entry.exists());
-            assertFalse(outputFile.exists());
-            assertTrue(expectedChangelog.exists());
+    void versionedUnreleasedDir(@TempDir Path tempDir) throws IOException {
+        // given:
+        Path VALID_PATH = copyToTempDir(PATH.resolve("versionedUnreleasedDir"), tempDir);
+        Path VALID_INPUT_DIR = VALID_PATH.resolve(INPUT_DIR);
 
-            // when:
-            GenerateProjectCommand.of(TEST_PATH, INPUT_DIR, TEST_PATH + "/" + OUTPUT_FILE, CONFIG_FILE).execute(false);
+        File entry = VALID_INPUT_DIR.resolve(UNRELEASED_VERSIONED).resolve(TEST_FILE).toFile();
+        File outputFile = VALID_PATH.resolve(OUTPUT_FILE).toFile();
+        File expectedChangelog = VALID_PATH.resolve("EXPECTED_CHANGELOG.md").toFile();
 
-            // then:
-            assertTrue(outputFile.exists());
-            String expectedContent = FileUtils.fileRead(expectedChangelog, "UTF-8");
-            String actualContent = FileUtils.fileRead(outputFile, "UTF-8");
-            assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
-        } finally {
-            cleanupMarkdownFiles(Paths.get(TEST_PATH));
-        }
-    }
+        assertTrue(entry.exists());
+        assertFalse(outputFile.exists());
+        assertTrue(expectedChangelog.exists());
 
-    private void cleanupMarkdownFiles(Path outputDir) throws IOException {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(outputDir, "*.md")) {
-            for (Path file : stream) {
-                if (file.endsWith("EXPECTED_CHANGELOG.md")) {
-                    continue;
-                }
-                System.out.println("Deleting file: " + file);
-                Files.delete(file);
-            }
-        }
+        // when:
+        GenerateProjectCommand.of(VALID_PATH.toString(), INPUT_DIR, VALID_PATH.resolve(OUTPUT_FILE).toString(), CONFIG_FILE).execute(false);
+
+        // then:
+        assertTrue(outputFile.exists());
+        String expectedContent = new String(Files.readAllBytes(expectedChangelog.toPath()), StandardCharsets.UTF_8);
+        String actualContent = new String(Files.readAllBytes(outputFile.toPath()), StandardCharsets.UTF_8);
+        assertThat(actualContent).isEqualToIgnoringWhitespace(expectedContent);
     }
 }
